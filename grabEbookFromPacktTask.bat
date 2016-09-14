740aeace459ff0abe7f3df8ed9f7d9597d65f6be
QuiZr <tymoteusz.adamczyk@outlook.com>
2016-09-14 21:03:33 +0200

    Added -l switch for logging.

--- grabEbookFromPacktTask.bat
+++ grabEbookFromPacktTask.bat
@@ -1,5 +1,13 @@
 @echo off
-echo *** Grabbing free eBook from Packt Publishing.... ***
-python packtPublishingFreeEbook.py -gd
-echo *** Done ! ***
-pause
+if "%1" == "-l" (
+    echo ***Date: %DATE:/=-% [%TIME::=:%] *** >> packtPublishingFreeEbook.log
+    echo *** Grabbing free eBook from Packt Publishing.... *** >> packtPublishingFreeEbook.log
+    python packtPublishingFreeEbook.py -gd >> packtPublishingFreeEbook.log 2>&1
+    echo:
+    echo:>> packtPublishingFreeEbook.log
+) ELSE (
+    echo *** Grabbing free eBook from Packt Publishing.... ***
+    python packtPublishingFreeEbook.py -gd
+    echo *** Done ! ***
+    pause
+)
